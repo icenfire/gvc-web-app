b0afaa95332b5778fe08becdf1280c8dfaba8179
chanyoungs <chanyoungs@gmail.com>
2019-06-13 02:36:07 +0100

    On signup, also made firestore document with the corresponding user id from auth

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -2,7 +2,7 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
 import React, { Fragment } from "react"
 
 import Authentication from "../auth/Authentication"
-import { db } from "../firebase"
+import { auth, db } from "../firebase"
 import AlreadyHaveAnAccount from "./Level1/Buttons/AlreadyHaveAnAccount"
 import DontHaveAnAccount from "./Level1/Buttons/DontHaveAnAccount"
 import SignInButton from "./Level1/Buttons/SignInButton"
@@ -41,7 +41,7 @@ function LoginPage(props: Props) {
     event: React.ChangeEvent<HTMLInputElement>
   ) => setValues({ ...values, [name]: event.target.value })
 
-  const signUpOnClick = () => {
+  const signUpOnClickTemp = () => {
     console.log("check")
     db.collection("members")
       .add({
@@ -58,6 +58,24 @@ function LoginPage(props: Props) {
           )
       })
       .catch(error => console.error("Error adding document: ", error))
+  }
+
+  const signUpOnClick = () => {
+    auth()
+      .createUserWithEmailAndPassword(values.email, values.pw)
+      .then(() => {
+        auth().onAuthStateChanged(user => {
+          if (user) {
+            db.collection("members").add({
+              userID: user.uid,
+              name: values.name,
+              dob: values.dob
+            })
+          }
+        })
+      })
+      .then(() => {})
+      .catch(error => console.error("Error signing up: ", error))
   }
 
   return (
