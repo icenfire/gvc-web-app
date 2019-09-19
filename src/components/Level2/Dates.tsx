a6476a7ee328e1a7e8cd03dbd0ab7b1c7b30e644
mcas <mcas1616@gmail.com>
2019-09-19 18:30:45 +0100

    working... from DB

--- src/components/Level2/Dates.tsx
+++ src/components/Level2/Dates.tsx
@@ -1,39 +1,54 @@
 import React, { Fragment } from "react"
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles'
-
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { red } from '@material-ui/core/colors';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     paper: {
       width: "100%"
     },
+    card: {
+      width: "100%"
+    },
     avatar: {
-      backgroundColor: red[500]
-    }
+      backgroundColor: red[500],
+    },
   }),
 )
 
 export interface Props {
-  
+  date: any
 }
 
 export interface State {
-  prayer: string
-  retreat: boolean
-  present: boolean
+  
 }
 
 function Dates(props: Props) {
   const classes = useStyles()
-  const [values, setValues] = React.useState<State>({
-    prayer:
-      "리더로서의 직분을 칙임감있게 감당할 수 있도록. 기도에 힘쓰고 매사에 성령님과 교제하는 삶을 살 수 있도록.",
-    retreat: false,
-    present: false
-  })
-  
+  const { date } = props;
+  console.log("Dates", date);
   return (
     <Fragment>
-      
+      <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={date}
+      />
+      </Card>
     </Fragment>
   )
 }
--- src/components/Pages/LeaderFormPage.tsx
+++ src/components/Pages/LeaderFormPage.tsx
@@ -1,20 +1,39 @@
-import * as React from "react"
-
+import React, { Fragment, useEffect, useState } from "react"
 import Authentication from "../../auth/Authentication"
 import SignOutButton from "../Level1/Buttons/SignOutButton"
 import PapersFull from "../Level2/Cards/PapersFull"
-
+import Dates from "../Level2/Dates";
+import { db } from "../../firebase";
+import { disconnect } from "cluster";
 // import CardsFull from "./Level2/Cards/CardsFull";
-export default class LoginPage extends React.PureComponent {
-  public render() {
-    return (
-      <>
-        <PapersFull />
-        <SignOutButton onClick={signOut} />
-      </>
+
+// export interface State {
+//   dates: string[]
+// }
+
+export default function LoginPage() {
+  // const [dates, setDates] = useState<State>({dates: []});
+  const [dates, setDates] = useState([]);
+
+
+  useEffect(() => {
+    console.log('useEffact');
+    
+    db.collection("dates").onSnapshot(snapshot => {
+      const datesDb : any = [];
+      snapshot.forEach(doc => { datesDb.push(doc.data().date)})
+      setDates(datesDb);
+    });
+    
+  });
+  return(
+    <Fragment>
+      <PapersFull />
+      <SignOutButton onClick={signOut} />
+      {/* <Dates date={date}/> */}
+    </Fragment>
     )
   }
-}
 
 function signOut() {
   Authentication.signOut()
