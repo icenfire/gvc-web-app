0aeb2f5b694013284648ac9ae72ba4d8f88fd6d8
mcas <mcas1616@gmail.com>
2020-06-08 16:23:07 +0100

    rollback firebase settup info

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,10 +1,17 @@
 import 'firebase/auth'
 import 'firebase/firestore'
-import { firebaseSetup } from '../config'
 
 import firebase from 'firebase/app'
 
-var config = firebaseSetup
+var config = {
+  apiKey: 'AIzaSyBQf0kjL5VgL9IJuEtqWHevRkOMTBmVwiE',
+  authDomain: 'london-gvc.firebaseapp.com',
+  databaseURL: 'https://london-gvc.firebaseio.com',
+  projectId: 'london-gvc',
+  storageBucket: 'london-gvc.appspot.com',
+  messagingSenderId: '139635867699',
+  appId: '1:139635867699:web:649f0b856111fee7'
+}
 
 export const app = firebase.initializeApp(config)
 export const auth = firebase.auth
